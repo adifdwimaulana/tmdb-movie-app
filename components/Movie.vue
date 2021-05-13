4de3a13d924f12ddc836c99c49db74a0929f2b3e
adifdwimaulana <adifdwimaulana@gmail.com>
2021-05-14 00:49:10 +0700

    Movie component

---
+++ components/Movie.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex flex-col items-center justify-center bg-white p-4 shadow rounded-lg">
+        <div class="inline-flex shadow-lg border border-gray-200 rounded-full overflow-hidden h-40 w-40">
+            <img :src="'https://image.tmdb.org/t/p/original' + imageUrl" class="h-full w-full">
+        </div>
+
+        <h2 class="mt-4 font-bold text-xl">{{title}}</h2>
+        <h6 class="text-sm font-normal">Release Date: {{releaseDate}}</h6>
+
+
+        <p class="text-xs text-gray-500 text-justify mt-3">
+            {{overview.slice(0, 200)}}<span class="font-bold hover:font-normal" style="cursor: pointer">&nbsp;Read more...</span>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Movie',
+    props: ['id', 'title', 'genre', 'releaseDate', 'imageUrl', 'overview']
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
         <div class="grid-rows-1 mt-4">
           <h4 class="text-lg font-bold">Genre</h4>
           <div v-for="genre in state.genres" v-bind:key="genre.id">
-            <input type="radio" v-model="state.selectedGenre" name="genre" value="genre.name" v-on:change="handleChangeGenre(genre.name)">&nbsp;{{genre.name}}
+            <input type="radio" v-model="state.selectedGenre" name="genre" value="genre.name" v-on:change="handleChangeGenre(genre.id)">&nbsp;{{genre.name}}
           </div>
           <!-- Filter Date -->
           <div class="grid-rows-1 mt-4">
@@ -46,7 +46,7 @@
               <button class="bg-gray-400 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded w-40" v-on:click="resetFilter()">
                 Reset Filter
               </button>
-              <button class="bg-gray-800 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded w-40" v-on:click="searchMovie()" style="margin-left: 100px">
+              <button class="bg-gray-800 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded w-40" v-on:click="applyFilter()" style="margin-left: 100px">
                 Apply Filter
               </button>
             </div>
@@ -57,10 +57,15 @@
         <h4 class="text-2xl font-bold">Hot Movies</h4>
 
         <div class="grid-rows-1 mt-4">
-
+          <div class="grid grid-cols-3 gap-4">
+            <Movie v-for="movie in state.movies" v-bind:key="movie.id"
+              :title="movie.title" :overview="movie.overview" :genre="movie.genre_ids"
+              :releaseDate="movie.release_date" :id="movie.id" :imageUrl="movie.poster_path"
+            />
+          </div>
         </div>
         <!-- Pagination -->
-        <div class="mt-5">
+        <div class="mt-5 mb-8">
           <ul class="flex pl-0 list-none rounded my-2">
             <li class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 rounded-l hover:bg-gray-200" v-on:click="handlePaginate(parseInt(state.page) - 1)">Previous</li>
             <li class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 hover:bg-gray-200" v-on:click="handlePaginate(1)">1</li>
@@ -81,6 +86,7 @@ import DatePicker from 'vue2-datepicker'
 import 'vue2-datepicker/index.css'
 import axios from 'axios'
 import moment from 'moment'
+import Movie from '../components/Movie.vue'
 
 export default {
   head() {
@@ -96,14 +102,15 @@ export default {
     }
   },
   components: {
-    DatePicker
+    DatePicker,
+    Movie,
   },
   setup(){
     const state = reactive({
       page: 1,
       movies: [],
       genres: [],
-      selectedGenre: '',
+      selectedGenre: 0,
       startTime: new Date(),
       endTime: new Date(),
       selectedRate: 50
@@ -131,40 +138,42 @@ export default {
 
       const responseMovies: any = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=9adbf9b696ac935f8dedfac5c6e8948b&language=en-US&page=${state.page}`)
       const resultMovies: any = await responseMovies.data.results
+      
+      state.movies = resultMovies
+    }
+
+    function handleChangeGenre(selectedGenre: number): void {
+      state.selectedGenre = selectedGenre
+    }
+
+    async function applyFilter(): Promise<any> {
+      const responseMovies: any = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=9adbf9b696ac935f8dedfac5c6e8948b&language=en-US&release_date.gte=${moment(state.startTime).format('YYYY-MM-DD')}&release_date.lte=${moment(state.endTime).format('YYYY-MM-DD')}&with_genres=${state.selectedGenre}&vote_average.gte=${state.selectedRate/10}`)
+      const resultMovies: any = await responseMovies.data.results
+      
+      state.movies = resultMovies
+    }
+
+    async function resetFilter(): Promise<any> {
+      state.selectedGenre = 0
+      state.startTime = new Date()
+      state.endTime = new Date()
+      state.selectedRate = 50
+
+      const responseMovies: any = await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=9adbf9b696ac935f8dedfac5c6e8948b&language=en-US&page=${state.page}`)
+      const resultMovies: any = await responseMovies.data.results
 
       state.movies = resultMovies
     }
 
-    function handleChangeGenre(selectedGenre: string): void {
-      state.selectedGenre = selectedGenre
-    }
-
-    function searchMovie(): void {
-      console.log(state.selectedGenre)
-      console.log(state.startTime, state.endTime)
-      console.log(state.selectedRate)
-    }
-
-    function resetFilter(): void {
-      state.selectedGenre = ''
-      state.startTime = new Date()
-      state.endTime = new Date()
-      state.selectedRate = 0
-    }
-
-    watch(() => state.startTime, (newValue, oldValue) => {
-      console.log(state.startTime)
-    })
-
-    watch(() => state.endTime, (newValue, oldValue) => {
-      console.log(state.endTime)
+    watch(() => state.movies, (newValue, oldValue) => {
+      console.log(state.movies)
     })
 
     return {
       state,
       handleChangeGenre,
       handlePaginate,
-      searchMovie,
+      applyFilter,
       resetFilter
     }
   }

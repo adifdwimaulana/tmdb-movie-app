6a8258f53cb4f975cf52dce4de16e69b1fc4b7e6
adifdwimaulana <adifdwimaulana@gmail.com>
2021-05-14 12:07:35 +0700

    login functionality

--- pages/login.vue
+++ pages/login.vue
@@ -2,6 +2,13 @@
     <div class="container mx-auto">
         <div class="my-20">
             <h4 class="font-2xl font-bold text-center">You're not logged in yet</h4>
+            <div class="mt-6" style="display: flex; flex-direction: row; justify-content: center;">
+                <nuxt-link to="/">
+                    <button class="bg-gray-800 hover:bg-blue-dark text-white mx-auto font-bold py-4 px-4 rounded" v-on:click="login()" style="width: 300px">
+                        Login without Credentials
+                    </button>
+                </nuxt-link>
+            </div>
         </div>
     </div>
 </template>
@@ -13,17 +20,15 @@ import cookies from 'cookie-universal-nuxt'
 export default {
     setup(app: any){
         onMounted(async (): Promise<any> => {
-            checkCookie
-
+            document.cookie = "isAuthenticated=false;"
         })
 
-        async function checkCookie(app: any): Promise<any> {
-            const auth = await app.$cookie.get('isAuthenticated') 
-            console.log(auth)
+        function login(): void {
+            document.cookie = "isAuthenticated=true"
         }
 
         return {
-            checkCookie
+            login
         }
     }
 }
